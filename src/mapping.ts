7a7f02a68ad21a633c7e894fec8c9657dd96137f
Hadrien Croubois <hc@iex.ec>
2019-09-19 18:41:12 +0200

    first commit

---
+++ src/mapping.ts
@@ -0,0 +1,13 @@
+import { EthereumBlock } from '@graphprotocol/graph-ts'
+import { Block, Transaction } from '../generated/schema'
+
+export function handleBlock(block: EthereumBlock): void {
+	let b = new Block(block.hash.toHex())
+	b.author    = block.author
+	b.number    = block.number
+	b.gasUsed   = block.gasUsed
+	b.gasLimit  = block.gasLimit
+	b.timestamp = block.timestamp
+	b.size      = block.size
+	b.save()
+}
